e9c6797276d1fbdd9d1e748f95256f34fd2d8f13
Nmplz <dartalex2@list.ru>
2025-03-23 02:56:23 +0300

    Preparation for final project.  Add __init__ file

---
+++ Autotests on Selenium and  Python/section_4_Final_project/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en", help="Choose language")
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Choose browser: chrome or firefox",
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("Running in Chrome..")
+        options = ChromeOptions()
+        options.add_experimental_option(
+            "prefs", {"intl.accept_languages": user_language}
+        )
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        print("Running in FireFox..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
