73c41c7ce1d752e861d9aaa6f594e2d729bd2675
Nmplz <dartalex2@list.ru>
2025-03-27 02:12:26 +0300

    Add some fixes to login form

--- Autotests on Selenium and  Python/section_4_Final_project/pages/locators.py
+++ Autotests on Selenium and  Python/section_4_Final_project/pages/locators.py
@@ -6,12 +6,15 @@ class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators:
-    LOGIN_EMAIL = (By.CSS_SELECTOR, "1#id_login-username")
-    LOGIN_PASSWORD = (By.CSS_SELECTOR, "1#id_login-password")
-    LOGIN_BUTTON = (By.CSS_SELECTOR, "1button[name='login_submit']")
+##TODO  СДЕЛАТЬ ПРОВЕРКУ НА ФОРМУ ЛОГИНА А НЕ НА ПОЛЯ И КНОПКИ
 
-    REGISTER_EMAIL = (By.CSS_SELECTOR, "1#id_registration-email")
-    REGISTER_PASSWORD = (By.CSS_SELECTOR, "1#id_registration-password1")
-    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "1#id_registration-password2")
-    REGISTER_BUTTON = (By.CSS_SELECTOR, "1button[name='registration_submit']")
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[name='login_submit']")
+
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
