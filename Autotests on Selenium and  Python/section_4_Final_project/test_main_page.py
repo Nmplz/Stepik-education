02ff358c471b3e34431a4f8ffb31143fcf3c9474
Nmplz <dartalex2@list.ru>
2025-03-27 00:38:13 +0300

    Add asserts and try except

--- Autotests on Selenium and  Python/section_4_Final_project/test_main_page.py
+++ Autotests on Selenium and  Python/section_4_Final_project/test_main_page.py
@@ -6,11 +6,17 @@ from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
 
-    page = MainPage(browser,link)
+    page = MainPage(browser, link)
     page.open()
     page.test_guest_can_go_to_login_page()
 
 
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
