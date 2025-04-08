9a6fcc2993f0989708f6e18ac4eedfbfcd5042c5
Nmplz <dartalex2@list.ru>
2025-04-09 00:06:17 +0300

    need_review marker

--- Autotests on Selenium and  Python/section_4_Final_project/test_product_page.py
+++ Autotests on Selenium and  Python/section_4_Final_project/test_product_page.py
@@ -62,6 +62,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -69,6 +70,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
 
@@ -92,12 +94,14 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, "Adminkit1")
         page.is_user_registred()
 
+    @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link, 0)
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
 
