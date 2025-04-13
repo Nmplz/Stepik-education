99e962d08c9b697e8960ef64d1e0aa42f2c139f8
Nmplz <dartalex2@list.ru>
2025-04-13 18:25:35 +0300

    Add test2 with Playwright locators

--- Playwright Python/test_todo.py
+++ Playwright Python/test_todo.py
@@ -1,19 +1,17 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
 
+#для запуска:  pytest --headed '.\Playwright Python\test_todo.py'
 
-def test_add_todo(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_add_todo(page) -> None:
+    
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
     page.get_by_role("checkbox", name="Toggle Todo").check()
     time.sleep(2)
-    # ---------------------
-    context.close()
-    browser.close()
+
+
 
 
