4eb873db3fac69bb9ac344f0621ef540c297807e
Nmplz <dartalex2@list.ru>
2025-05-04 17:40:50 +0300

    add ..

---
+++ Playwright Python/10.py
@@ -0,0 +1,6 @@
+GET / HTTP/1.1 // GET — запрос данных методом GET; / — запрос на главной странице; HTTP/1.1 — протокол HTTP версии 1.1;
+
+Host: [ya.ru](http://ya.ru/){target="_blank"} // ya.ru — хост, к которому фронтенд обращается с запросом;
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 YaBrowser/21.6.0.620 Yowser/2.5 Safari/537.36 // здесь информация о браузерах, операционной системе и версиях ПО клиента;
+Accept-Language: ru,en // ru,en — допустимые языки в ответе;
+Connection: keep-alive // keep-alive — постоянное соединение с сервером;
